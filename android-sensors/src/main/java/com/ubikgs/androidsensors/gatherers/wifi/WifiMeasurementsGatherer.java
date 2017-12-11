6f01a15b21c97150b14fe1d7a5604d3f92b91bfa
Alberto Gonzalez Perez <algonzal@uji.es>
2017-12-11 10:16:38 +0100

    Now WifiMeasurementsGatherer immediately asks for a new scan if more time than the specified has passed

--- android-sensors/src/main/java/com/ubikgs/androidsensors/gatherers/wifi/WifiMeasurementsGatherer.java
+++ android-sensors/src/main/java/com/ubikgs/androidsensors/gatherers/wifi/WifiMeasurementsGatherer.java
@@ -16,6 +16,7 @@ import com.ubikgs.androidsensors.gatherers.AbstractSensorGatherer;
 import com.ubikgs.androidsensors.records.SensorRecord;
 import com.ubikgs.androidsensors.records.wifi.WifiMeasurementsRecord;
 
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -69,13 +70,27 @@ public class WifiMeasurementsGatherer extends AbstractSensorGatherer {
     private BroadcastReceiver defineBroadcastReceiverFor(final FlowableEmitter<SensorRecord> subscriber){
         return new BroadcastReceiver() {
             Timer timer = new Timer();
+            long prevCallTime = new Date().getTime();
 
             @Override
             public void onReceive(Context context, Intent intent) {
+                long actualTime = new Date().getTime();
+                long delay = calculateDiffDelay(prevCallTime, actualTime);
+                prevCallTime = actualTime;
+
                 subscriber.onNext(new WifiMeasurementsRecord(wifiManager.getScanResults()));
-                timer.schedule(createScanTask(), sensorConfig.getMinSensorDelay(SensorType.WIFI_MEASUREMENTS));
+
+                if (delay > 0)
+                    timer.schedule(createScanTask(), delay);
+                else
+                    createScanTask().run();
             }
         };
+    }
+
+    protected long calculateDiffDelay(long prevCallTime, long actualTime) {
+        long diff = actualTime - prevCallTime;
+        return sensorConfig.getMinSensorDelay(SensorType.WIFI_MEASUREMENTS) - diff;
     }
 
     private void registerBroadcastReceiver(BroadcastReceiver broadcastReceiver){
